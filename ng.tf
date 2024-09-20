b079108e6ef7d751450e7eb13b4360377ee1ac98
sreeram <sreeramguguloth21@gmail.com>
2024-09-21 00:05:48 +0530

    5

--- ng.tf
+++ ng.tf
@@ -1,7 +1,10 @@
 provider "kubernetes" {
-    config_path = "~./kube/config"
+    config_path = "/home/ram/.kube/config"
 }
+
 resource "kubernetes_manifest" "ng" {
     manifest = yamldecode(file("/home/ram/terraform/terraform_manifest/pod.yaml"))
-  
+}
+resource "kubernetes_manifest" "service" {
+  manifest = yamldecode(file("/home/ram/terraform/terraform_manifest/service.yaml"))
 }
